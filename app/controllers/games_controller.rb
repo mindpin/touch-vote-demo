eda7de7632694cc34be1baafe454aebbdfbad7aa
arlyxiao <kingla_pei@163.com>
2015-04-17 15:01:39 +0800

    fix

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,12 +3,12 @@ class GamesController < ApplicationController
 
   def index
     # jd test code
-    jd = Jd.new
-    @jd_oauth_link = jd.build_url
+    # jd = Jd.new
+    # @jd_oauth_link = jd.build_url
 
-    data = jd.get_access_token(params['code'])
+    # data = jd.get_access_token(params['code'])
 
-    JdUser.create(:token => data['access_token'])
+    # JdUser.create(:token => data['access_token'])
   end
 
 
